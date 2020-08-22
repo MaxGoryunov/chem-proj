9f2c7daeee1e21c44ea3cb4b6f00329725dca093
Max Goryunov <maximthehedgehog@yandex.ru>
2020-08-23 01:11:56 +0300

    now the development of this project will be accompanied by the documentation of the classes, functions, variables etc

--- components/Autoloader.php
+++ components/Autoloader.php
@@ -2,8 +2,13 @@
 
 	class Autoloader {
 
-		public static function register() {
+		public static function register():void {
 			spl_autoload_register(function(string $className):void {
+				/**
+				 * Directories in which the class, interface or trait file is present
+				 * 
+				 * @var string[]
+				 */
 				$dirs = array("components", "controllers", "models", "views");
 				
 				foreach ($dirs as $dir) {
