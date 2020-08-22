d4371a1c5aa7bbeb8c1e3849c4584e97847667c4
Max Goryunov <maximthehedgehog@yandex.ru>
2020-08-23 00:29:29 +0300

    rewrote Autoloader with a more OOP approach

--- components/Autoloader.php
+++ components/Autoloader.php
@@ -1,14 +1,20 @@
 <?php
 
-	spl_autoload_register(function(string $className):void {
-        $dirs = array("components", "controllers", "models", "views");
-        
-		foreach ($dirs as $dir) {
-            $filePath = "./$dir/$className.php";
-            
-			if (file_exists($filePath)) {
-				include_once($filePath);
-				break;
-			}
+	class Autoloader {
+
+		public static function register() {
+			spl_autoload_register(function(string $className):void {
+				$dirs = array("components", "controllers", "models", "views");
+				
+				foreach ($dirs as $dir) {
+					$filePath = "./$dir/$className.php";
+					
+					if (file_exists($filePath)) {
+						include_once($filePath);
+		
+						break;
+					}
+				}
+			});
 		}
-	});
+	}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     include_once("./components/Autoloader.php");
     include_once("./config/constants.php");
 
+    Autoloader::register();
+
     $router = new Router();
 
     $router->run();
