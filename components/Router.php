c8270b885a5525b580749a8edf0acc45c068d73f
Max Goryunov <maximthehedgehog@yandex.ru>
2020-08-23 02:11:39 +0300

    added documentation to Router

--- components/Router.php
+++ components/Router.php
@@ -1,6 +1,11 @@
 <?php
 
     class Router {
+		/**
+		 * Routes for controller actions
+		 *
+		 * @var string[][]
+		 */
         private $routes;
 
         public function __construct() {
@@ -10,15 +15,23 @@
         }
 
         public function run():void {
-            $userUri = $_SERVER['REQUEST_URI'];
+			$userUri = $_SERVER['REQUEST_URI'];
 
+			/**
+			 * @todo Implement a more efficient algorithm as this works for O(n) where n is the overall number of patterns
+			 */
             foreach ($this->routes as $controller => $patterns) {
 				foreach ($patterns as $pattern => $action) {
                     $completePattern = ROOT . $pattern;
                     
 					if (preg_match("~$completePattern$~", $userUri, $matches)) {
-
+						/**
+						 * If the id of some item is specified in the URL then it is contained in the first submask
+						 * 
+						 * @var string $id
+						 */
 						$id = isset($matches[1]) ? $matches[1] : '';
+						
 						$controllerObj = new $controller();
 
 						if ($id) {
