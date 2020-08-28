51ba3c0c558eaf2031edc5ba7d47771471806105
Max Goryunov <maximthehedgehog@yandex.ru>
2020-08-28 20:04:01 +0300

    created Query and QueryBuilder interfaces, implemented Query class

---
+++ dbQueries/Query.php
@@ -0,0 +1,32 @@
+<?php
+
+    /**
+     * Class for holding a query string to DB
+     */
+    class Query implements IQuery {
+        
+        /**
+         * Holds a string value
+         *
+         * @var string
+         */
+        private $queryString = "";
+
+        /**
+         * Accepts the string through a constructor so that it cannot be changed later
+         *
+         * @param string $queryString
+         */
+        public function __construct(string $queryString) {
+            $this->queryString = $queryString;
+        }
+
+        /**
+         * Returns a query string
+         *
+         * @return string
+         */
+        public function getQueryString():string {
+            return $this->queryString;
+        }
+    }
