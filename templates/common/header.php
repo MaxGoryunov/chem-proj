3e2d277684dd057b04ed6a02f90de4380e6fbd63
Max Goryunov <maximthehedgehog@yandex.ru>
2020-08-31 20:35:04 +0300

    modified header template to access custom css files

--- templates/common/header.php
+++ templates/common/header.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= LIBS; ?>/bootstrap/css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="<?= LIBS; ?>bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= CSS; ?>common/header.css">
+    <link rel="stylesheet" href="<?= CSS . $cssFile; ?>.css">
+    <title><?= $title; ?></title>
 </head>
 <body>
     
