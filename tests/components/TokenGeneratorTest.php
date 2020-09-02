dd649193879b7c22c176eed096020a85cdc4355c
Max Goryunov <maximthehedgehog@yandex.ru>
2020-09-02 20:19:30 +0300

    added TokenGeneratorTest

---
+++ tests/components/TokenGeneratorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+    include_once("./components/TokenGenerator.php");
+
+    class TokenGeneratorTest extends TestCase {
+
+        /**
+         * Contains tested class
+         *
+         * @var TokenGenerator
+         */
+        protected $tokenGenerator;
+        
+        protected function setUp():void {
+            $this->tokenGenerator = new TokenGenerator();
+        }
+
+        protected function tearDown():void {
+            $this->tokenGenerator = null;
+        }
+        
+        /**
+         * @dataProvider provideTokenLength
+         *
+         * @param int $length - length of the Token
+         * @param int $expected - expected Token length
+         * @return void
+         */
+        public function testTokenLength(int $length, int $expected):void {
+            self::assertEquals($expected, strlen($this->tokenGenerator->generateToken($length)));
+        }
+
+        public function provideTokenLength():array {
+            return [
+                "negative" => [-3, 0],
+                "zero"     => [0, 0],
+                "positive" => [20, 20]
+            ];
+        }
+    }
