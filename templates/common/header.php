63b071e8ba5dc8f1bcbcbd1b99cec4339dd1e635
Max Goryunov <maximthehedgehog@yandex.ru>
2020-09-13 19:17:43 +0300

    revowrked header styles; added fontawesome icons to header.php

--- templates/common/header.php
+++ templates/common/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?= LIBS; ?>bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= LIBS; ?>/fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="<?= LIBS; ?>/fontawesome/css/solid.css">
     <link rel="stylesheet" href="<?= CSS; ?>common/header.css">
     <link rel="stylesheet" href="<?= CSS; ?>common/page.css">
     <link rel="stylesheet" href="<?= CSS; ?>common/index.css">
@@ -18,9 +20,26 @@
         <div class="header__logoblock"></div>
         <form action="" class="header__form"></form>
         <div class="header__navblock">
-            <div class="header__navitem">Lorem, ipsum.</div>
-            <div class="header__navitem">Eum, ipsum.</div>
-            <div class="header__navitem">Reprehenderit, alias.</div>
-            <div class="header__navitem">Laborum, doloribus?</div>
+            <div class="header__navitem">
+                <a href="" class="header__link">
+                    <div class="header__icon"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="header__text">Адреса</div>
+                </a>
+            </div><div class="header__navitem">
+                <a href="" class="header__link">
+                    <div class="header__icon"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="header__text">Адреса</div>
+                </a>
+            </div><div class="header__navitem">
+                <a href="" class="header__link">
+                    <div class="header__icon"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="header__text">Адреса</div>
+                </a>
+            </div><div class="header__navitem">
+                <a href="" class="header__link">
+                    <div class="header__icon"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="header__text">Адреса</div>
+                </a>
+            </div>
         </div>
     </header>
