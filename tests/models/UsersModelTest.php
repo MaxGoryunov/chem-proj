1199ae3a967faa974c0d0c40b9b2e48a2bcb9179
Max Goryunov <maximthehedgehog@yandex.ru>
2020-09-19 00:34:01 +0300

    added UsersModelTest with 1 test

---
+++ tests/models/UsersModelTest.php
@@ -0,0 +1,60 @@
+<?php
+
+    namespace Tests\Models;
+
+    use Factories\AbstractMVCPDMFactory;
+    use Models\UsersModel;
+    use PHPUnit\Framework\TestCase;
+
+    /**
+     * Testing UsersModel class
+     * 
+     * @coversDefaultClass UsersModel
+     */
+    class UsersModelTest extends TestCase {
+
+        /**
+         * Contains tested class object
+         *
+         * @var UsersModel
+         */
+        protected $usersModel;
+
+        /**
+         * Creates tested class object
+         *
+         * @return void
+         */
+        protected function setUp():void {
+            $this->usersModel = new UsersModel($this->getMockForAbstractClass(AbstractMVCPDMFactory::class));
+        }
+
+        /**
+         * Removes tested class object
+         *
+         * @return void
+         */
+        protected function tearDown():void {
+            $this->usersModel = null;
+        }
+
+        /**
+         * @covers ::getUserAdminStatus
+         * 
+         * @dataProvider provideUserIds
+         *
+         * @param int $id
+         * @return void
+         */
+        public function testGetUserAdminStatusReturnsCorrectValue(int $id, bool $expected):void {
+            $this->assertEquals($expected, $this->usersModel->getUserAdminStatus($id));
+        }
+
+        public function provideUserIds():array {
+            return [
+                "none"         => [0, false],
+                "regular user" => [3, false],
+                "admin"        => [12, true]
+            ];
+        }
+    }
