c129f2febb9f151e32dabe80257a94cc9d6cbfdc
Max Goryunov <maximthehedgehog@yandex.ru>
2020-09-21 03:27:16 +0300

    added 'getUserAdminStatus' implementation

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -2,7 +2,10 @@
 
     namespace Models;
 
-    use Entities\IEntity;
+use Components\DBConnectionProvider;
+use Components\IDBConnection;
+use DBQueries\SelectQueryBuilder;
+use Entities\IEntity;
     use Entities\UserEntity;
 
     /**
@@ -53,18 +56,28 @@
         }
 
         /**
-         * @todo Implement a stricter and safer version of the method with SESSION instead of COOKIE
-         */
-        /**
-         * @todo Implement a complete version of the method
-         */
-        /**
-         * Returns User's Admin Status based on User's COOKIE
+         * Returns User's Admin Status based on User's SESSION
          *
-         * @return boolean
+         * @param int $userId
+         * @return bool
          */
-        public function getUserAdminStatus():bool {
-            return false;
+        public function getUserAdminStatus(int $userId = 0):bool {
+            if ($userId == 0) {
+                return false;
+            }
+
+            $connection = DBConnectionProvider::getConnection(IDBConnection::class);
+            $what       = ["user_is_admin"];
+
+            $query      = (new SelectQueryBuilder($this->getTableName()))
+                          ->what($what)
+                          ->whereAnd("`user_id` = " . $userId)
+                          ->build();
+
+            $res         = mysqli_query($connection, $query->getQueryString());
+            $userIsAdmin = mysqli_fetch_assoc($res)['user_is_admin'];
+
+            return (bool)$userIsAdmin;
         }
 
         /**
