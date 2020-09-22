0267579ab00851434926474b0323b20942535fdb
Max Goryunov <maximthehedgehog@yandex.ru>
2020-09-22 18:06:49 +0300

    removed Singleton methods and made connection a static property

--- components/MySQLConnection.php
+++ components/MySQLConnection.php
@@ -1,94 +1,50 @@
 <?php
 
     namespace Components;
-    
+
+    use Exception;
+    use mysqli;
+
     /**
      * Class which represents connection with MySQL Database
      * 
      * This class implements Singleton Pattern
      */
     class MySQLConnection implements IDBConnection {
-        /**
-         * Singleton instance
-         *
-         * @static
-         * 
-         * @var self|null
-         */
-        private static $instance = null;
 
         /**
          * Connection to MySQL Database
          *
-         * @var \mysqli
+         * @var mysqli
          */
-        private $connection = null;
-
-        /**
-         * Function returns the instance of the class which uses this trait
-         *
-         * @static
-         * 
-         * @return self
-         */
-        public static function getInstance():self {
-            if (!self::$instance) {
-                self::$instance = new self();
-            }
-            return self::$instance;
-        }
-
-        /**
-         * Restricting the creation of class instance outside of class
-         * 
-         * @throws Exception if the connection to MySQL failed
-         */
-        private function __construct() {
-            include_once("./config/dbConfig.php");
-
-            /**
-             * Establishing the connection with MySQL Database
-             */
-            $connection = new \mysqli($dbConfig["host"], $dbConfig["user"], $dbConfig["password"], $dbConfig["database"]);
-
-            if ($connection->connect_error) {
-                throw new \Exception("Failed to connect to MySQL database: " . $connection->connect_errno);
-            }
-
-            $connection->set_charset($dbConfig["charset"]);
-
-            $this->connection = $connection;
-
-            return;
-        }
+        private static $connection = null;
 
         /**
          * Returns the MySQL Database connection
+         * 
+         * @throws Exception if the connection to MySQL has failed
          *
-         * @return \mysqli
+         * @return mysqli
          */
-        public function getConnection():\mysqli {
-            return $this->connection;
+        public function getConnection():mysqli {
+            if (!self::$connection) {
+                include_once("./config/dbConfig.php");
+
+                /**
+                 * Establishing the connection with MySQL Database
+                 */
+                $connection = new mysqli($dbConfig["host"], $dbConfig["user"], $dbConfig["password"], $dbConfig["database"]);
+
+                if ($connection->connect_error) {
+                    throw new Exception("Failed to connect to MySQL database: " . $connection->connect_errno);
+                }
+
+                $connection->set_charset($dbConfig["charset"]);
+
+                self::$connection = $connection;
+            }
+
+            return self::$connection;
         }
 
-        /**
-         * Restricting cloning
-         */
-        private function __clone() {
-
-        }
-
-        /**
-         * Restricting serialization
-         */
-        private function __wakeup() {
-
-        }
-
-        /**
-         * Restricting deserialization
-         */
-        private function __sleep() {
-
-        }
     }
