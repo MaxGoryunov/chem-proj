7e6b3d14ea346bac88682dc3642cff12f8a05df3
Max Goryunov <maximthehedgehog@yandex.ru>
2020-11-19 23:37:56 +0300

    removed br tags in add.php

--- templates/genders/add.php
+++ templates/genders/add.php
@@ -3,11 +3,11 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label class="container__formlabel" for=""> Название: </label>
-            <input class="form-control container__input" type="text" name="gender_name" required><br>
+            <input class="form-control container__input" type="text" name="gender_name" required>
         </div>
         <div class="form-group">
             <label class="container__formlabel" for=""> Краткое название: </label>
-            <input class="form-control container__input" type="text" name="gender_short_name" required><br>
+            <input class="form-control container__input" type="text" name="gender_short_name" required>
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__actionbutton" value="Добавить">
     </form>
