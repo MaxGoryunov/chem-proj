6a7d4d92f1adc30f6f9fe9937961bcd9810b7844
Max Goryunov <maximthehedgehog@yandex.ru>
2020-11-29 00:03:09 +0300

    added 'notAdmin' page

---
+++ templates/errors/notAdmin.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="container__error-sign">
+        <i class="fas fa-exclamation"></i>
+        <i class="fas fa-crown"></i>
+    </div>
+    <h2 class="container__header">Oopsie!</h2>
+    <div class="container__description">
+        <p>Для открытия данной страницы необходимы права администратора.</p>
+        <a href="../medicines/list" class="btn btn-link" role="button">Вернуться на главную страницу</a>
+    </div>
+</div>
+
+
+
