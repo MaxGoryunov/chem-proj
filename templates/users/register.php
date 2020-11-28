9e4871ded82363114f70a5e0048a81646278f2c3
Max Goryunov <maximthehedgehog@yandex.ru>
2020-11-29 00:10:50 +0300

    removed br tags in register

--- templates/users/register.php
+++ templates/users/register.php
@@ -3,31 +3,31 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label for="" class="container__formlabel"> Имя:</label>
-            <input class="form-control container__input" type="text" name="user_name"> <br>
+            <input class="form-control container__input" type="text" name="user_name">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Фамилия: </label>
-            <input class="form-control container__input" type="text" name="user_surname"> <br>
+            <input class="form-control container__input" type="text" name="user_surname">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Логин: </label>
-            <input class="form-control container__input" type="text" name="user_login"> <br>
+            <input class="form-control container__input" type="text" name="user_login">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Email: </label>
-            <input class="form-control container__input" type="text" name="user_email"> <br>
+            <input class="form-control container__input" type="text" name="user_email">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Телефон: </label>
-            <input class="form-control container__input" type="tel" name="user_phone"> <br>
+            <input class="form-control container__input" type="tel" name="user_phone">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Пароль: </label>
-            <input class="form-control container__input" type="password" name="user_password"> <br>
+            <input class="form-control container__input" type="password" name="user_password">
         </div>
         <div class="form-group">
             <label for="" class="container__formlabel"> Пароль еще раз: </label>
-            <input class="form-control container__input" type="password" name="repeat_password"> <br>
+            <input class="form-control container__input" type="password" name="repeat_password">
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__actionbutton" value="Зарегистрироваться">
     </form>
