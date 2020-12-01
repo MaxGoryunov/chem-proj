80f0913373cffef7e4b7cef172d5ded4110ccab0
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-01 23:48:40 +0300

    removed br tags in add.php

--- templates/addresses/add.php
+++ templates/addresses/add.php
@@ -3,7 +3,7 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label class="container__formlabel" for=""> Название: </label>
-            <input class="form-control container__input" type="text" name="address_name" required><br>
+            <input class="form-control container__input" type="text" name="address_name" required>
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__action-button" value="Добавить">
     </form>
