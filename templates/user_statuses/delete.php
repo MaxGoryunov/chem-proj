d7e77bc0da3e5e5066d5469463be3f73773b80b5
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-01 23:46:14 +0300

    removed br tags in delete.php

--- templates/user_statuses/delete.php
+++ templates/user_statuses/delete.php
@@ -3,7 +3,7 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label class="container__formlabel" for=""> Название: </label>
-            <input readonly class="form-control form-control-plaintext container__input" type="text" name="user_status_name" value="<?= $userStatus['user_status_name']; ?>" required><br>
+            <input readonly class="form-control form-control-plaintext container__input" type="text" name="user_status_name" value="<?= $userStatus['user_status_name']; ?>" required>
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__actionbutton" value="Удалить">
     </form>
