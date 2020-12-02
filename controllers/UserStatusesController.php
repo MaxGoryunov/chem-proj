bf70a78a0a3059d82f494f9049b9e471435e4a9a
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-03 00:53:14 +0300

    added 'edit' method in UserStatusesController

--- controllers/UserStatusesController.php
+++ controllers/UserStatusesController.php
@@ -26,8 +26,24 @@ use Factories\UsersFactory;
             $this->getView()->render(__FUNCTION__, $viewData);
         }
 
+        /**
+         * {@inheritDoc}
+         */
         public function edit(int $id):void {
+            $title          = "Редактирование статуса пользователя";
+            $userStatus     = $this->getModel()->getById($id);
+			$fullUserStatus = (new UsersFactory())->getModel()->getUserFullStatus();
+			
+			if (isset($_POST["name"])) {
+				$name = $_POST["name"];
+                $data = compact("name", "id");
+                
+                $this->getModel()->edit($data);
+            }
             
+            $viewData = array_merge($fullUserStatus, compact("title", "userStatus"));
+            
+            $this->getView()->render(__FUNCTION__, $viewData);
         }
 
         /**
