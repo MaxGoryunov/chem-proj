1f230b366f0d71cabaf23aab0ac33cc8b5f9056c
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-06 00:23:09 +0300

    added 'notAuthorised' template

--- index.php
+++ index.php
@@ -25,4 +25,4 @@
     $router->run();
 
     include_once("./templates/common/header.php");
-    include_once("./templates/errors/notAdmin.php");
+    include_once("./templates/errors/notAuthorised.php");
---
+++ templates/errors/notAuthorised.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="container__error-sign">
+        <i class="fas fa-exclamation"></i>
+        <i class="fas fa-user-alt"></i>
+    </div>
+    <h2 class="container__header">Oopsie!</h2>
+    <div class="container__description">
+        <p>Для доступа к данной странице необходимо авторизоваться</p>
+        <a href="../users/authorisation" class="btn btn-link" role="button">Авторизация</a>
+    </div>
+</div>
