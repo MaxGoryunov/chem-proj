46899b304db2f0d3b3252d8cfcbf8f3809692466
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-10 20:18:21 +0300

    removed br tags in edit.php

--- templates/genders/edit.php
+++ templates/genders/edit.php
@@ -3,11 +3,11 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label  class="container__formlabel" for=""> Название: </label>
-            <input class="form-control container__input" type="text" name="gender_name" value="<?= $gender['gender_name']; ?>"> <br>
+            <input class="form-control container__input" type="text" name="gender_name" value="<?= $gender['gender_name']; ?>">
         </div>
         <div class="form-group">
             <label  class="container__formlabel" for=""> Краткое название: </label>
-            <input class="form-control container__input" type="text" name="gender_short_name" value="<?= $gender['gender_short_name']; ?>"> <br>
+            <input class="form-control container__input" type="text" name="gender_short_name" value="<?= $gender['gender_short_name']; ?>">
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__actionbutton" value="Сохранить">
     </form>
