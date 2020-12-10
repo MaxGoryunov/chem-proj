d3de492cfa8d695863cbc51028dad6bdfa6afc4f
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-10 20:22:16 +0300

    removed br tags in edit.php

--- templates/user_statuses/edit.php
+++ templates/user_statuses/edit.php
@@ -3,7 +3,7 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label  class="container__formlabel" for=""> Название: </label>
-            <input class="form-control container__input" type="text" name="user_status_name" value="<?= $userStatus['user_status_name']; ?>"> <br>
+            <input class="form-control container__input" type="text" name="user_status_name" value="<?= $userStatus['user_status_name']; ?>">
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__actionbutton" value="Сохранить">
     </form>
