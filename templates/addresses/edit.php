55da69eb61eb722302952e0aae95dc39faaa2de1
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-17 17:34:42 +0300

    removed  br tags in edit.php

--- templates/addresses/edit.php
+++ templates/addresses/edit.php
@@ -3,7 +3,7 @@
     <form class="container__form" method="POST">
         <div class="form-group">
             <label  class="container__formlabel" for=""> Название: </label>
-            <input class="form-control container__input" type="text" name="address_name" value="<?= $address['address_name']; ?>"> <br>
+            <input class="form-control container__input" type="text" name="address_name" value="<?= $address['address_name']; ?>">
         </div>
         <input type="submit" class="btn btn-outline-primary btn-lg container__action-button" value="Сохранить">
     </form>
