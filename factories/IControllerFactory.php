4945d55cc9921f0054462e7ba5cd575bce5f94aa
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-19 22:03:10 +0300

    extracted Controller creation into a separate interface

--- factories/IControllerFactory.php
+++ factories/IControllerFactory.php
@@ -3,16 +3,11 @@
     namespace Factories;
 
     use Controllers\IController;
-    use DataMappers\IDataMapper;
-    use Models\IModel;
-    use Views\IView;
 
     /**
-     * This interface is used by all factories
-     * 
-     * MVCPDM stands for Model - View - Controller - Proxy - Data Mapper
+     * interface for factories which can create Controllers
      */
-    interface IMVCPDMFactory {
+    interface IControllerFactory {
 
         /**
          * Returns a specified Controller
