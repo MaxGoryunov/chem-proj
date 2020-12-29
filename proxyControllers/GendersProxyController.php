99489ddbbc29cbe6d9d5a4fa08a9bb1fbc23e70b
Max Goryunov <maximthehedgehog@yandex.ru>
2020-12-30 01:42:00 +0300

    added 'add', 'edit' and 'delete' methods in GendersProxyController

--- proxyControllers/GendersProxyController.php
+++ proxyControllers/GendersProxyController.php
@@ -2,6 +2,8 @@
 
     namespace ProxyControllers;
 
+    use Factories\UsersFactory;
+
     /**
      * Class which replaces GendersController on class calls
      */
@@ -18,20 +20,32 @@
          * {@inheritDoc}
          */
         public function add():void {
-            
+            $userAdminStatus = (new UsersFactory())->getModel()->getUserAdminStatus();
+
+            if ($userAdminStatus) {
+                $this->getController()->add();
+            }
         }
 
         /**
          * {@inheritDoc}
          */
         public function edit(int $id):void {
-            
+            $userAdminStatus = (new UsersFactory())->getModel()->getUserAdminStatus();
+
+            if ($userAdminStatus) {
+                $this->getController()->edit($id);
+            }
         }
 
         /**
          * {@inheritDoc}
          */
         public function delete(int $id):void {
-            
+            $userAdminStatus = (new UsersFactory())->getModel()->getUserAdminStatus();
+
+            if ($userAdminStatus) {
+                $this->getController()->delete($id);
+            }
         }
     }
