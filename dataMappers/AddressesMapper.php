278313dd63a4c77a456243e1404ed0dba87f5746
Max Goryunov <maximthehedgehog@yandex.ru>
2021-01-04 03:17:31 +0300

    fetched files from crud-addresses; added 'mapQueryResultToEntity' method in UsersMapper

--- dataMappers/AddressesMapper.php
+++ dataMappers/AddressesMapper.php
@@ -1,15 +1,30 @@
 <?php
 
     namespace DataMappers;
-    
+
+    use Components\DBConnectionProvider;
+    use Components\IDBConnection;
+    use DBQueries\SelectQueryBuilder;
     use Entities\AddressEntity;
+    use Entities\IEntity;
 
     /**
      * Data Mapper for mapping AddressEntity objects
      */
     class AddressesMapper implements IDataMapper {
         
-        public function mapDatasetToEntity(array $data = []):AddressEntity {
-            return new AddressEntity();
+        /**
+         * {@inheritDoc}
+         * 
+         * @return AddressEntity
+         */
+        public function mapQueryResultToEntity(SelectQueryBuilder $builder):IEntity {
+            $connection = DBConnectionProvider::getConnection(IDBConnection::class);
+            $query      = $builder->build();
+
+            $result = $connection->query($query->getQueryString());
+            $entity = $result->fetch_object(AddressEntity::class);
+
+            return $entity;
         }
     }
--- dataMappers/UsersMapper.php
+++ dataMappers/UsersMapper.php
@@ -2,12 +2,28 @@
 
     namespace DataMappers;
 
-    use Entities\IEntity;
+    use Components\DBConnectionProvider;
+    use Components\IDBConnection;
+    use DBQueries\SelectQueryBuilder;
     use Entities\UserEntity;
 
+    /**
+     * Data Mapper for mapping User Entity objects
+     */
     class UsersMapper extends AbstractDataMapper {
 
-        public function mapDatasetToEntity(array $data = []):IEntity {
-            return new UserEntity();
+        /**
+         * {@inheritDoc}
+         * 
+         * @return UserEntity
+         */
+        public function mapQueryResultToEntity(SelectQueryBuilder $builder):UserEntity {
+            $connection = DBConnectionProvider::getConnection(IDBConnection::class);
+            $query      = $builder->build();
+
+            $result = $connection->query($query->getQueryString());
+            $entity = $result->fetch_object(UserEntity::class);
+
+            return $entity;
         }
     }
