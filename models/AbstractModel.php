9cee4d137f35e16cbcbda60d74da183acb6b6c6b
Max Goryunov <maximthehedgehog@yandex.ru>
2021-03-25 01:12:34 +0300

    added 'getTableName' method in AbstractModel

--- models/AbstractModel.php
+++ models/AbstractModel.php
@@ -6,5 +6,20 @@
      * Base class for implementing other Models
      */
     abstract class AbstractModel {
-        
+
+        /**
+         * Table name to which the model belongs
+         *
+         * @var string
+         */
+        private $tableName;
+
+        /**
+         * Returns table name
+         *
+         * @return string
+         */
+        public function getTableName():string {
+            return $this->tableName;
+        }
     }
