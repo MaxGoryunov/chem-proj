62da9a5fc1d7d704e63d0dc87e300772f6790ab7
Max Goryunov <maximthehedgehog@yandex.ru>
2021-06-12 18:19:46 +0300

    refactored DomainModel

--- models/DomainModel.php
+++ models/DomainModel.php
@@ -80,9 +80,7 @@ use DBQueries\InsertQueryBuilder;
          * @return AbstractDataMapper
          */
         protected function getDataMapper():DomainMapper {
-            if (!isset($this->relatedMapper)) {
-                $this->relatedMapper = $this->relatedFactory->getDataMapper();
-            }
+            $this->relatedMapper ??= $this->relatedFactory->getDataMapper();
 
             return $this->relatedMapper;
         }
@@ -100,68 +98,56 @@ use DBQueries\InsertQueryBuilder;
          * {@inheritDoc}
          */
         public function getList(int $limit, int $offset):array {
-            $connection = $this->connectToDB();
-
-            $builder    = (new SelectQueryBuilder($this))
-                            ->where("`" . $this->getDomainName() . "_is_deleted` = 0")
-                            ->limit($limit, $offset);
-
-            $result    = $connection->query($builder);
-            $itemsList = $result->fetch_all(MYSQLI_ASSOC);
-
-            return $itemsList;
+            return $this->connectToDB()->query(
+                (new SelectQueryBuilder($this))
+                ->where("`" . $this->getDomainName() . "_is_deleted` = 0")
+                ->limit($limit, $offset)
+            )->fetch_all(MYSQLI_ASSOC);
         }
 
         /**
          * {@inheritDoc}
          */
         public function getById(int $id):IEntity {
-            $builder = (new SelectQueryBuilder($this))
-                        ->where("`" . $this->getDomainName() . "_is_deleted` = 0")
-                        ->and("`" . $this->getDomainName() . "_id` = " . $id);
-
-            $entity = $this->getDataMapper()->mapQueryResultToEntity($builder);
-
-            return $entity;
+            return $this->getDataMapper()->mapQueryResultToEntity(
+                (new SelectQueryBuilder($this))
+                ->where("`" . $this->getDomainName() . "_is_deleted` = 0")
+                ->and("`" . $this->getDomainName() . "_id` = " . $id)
+            );
         }
 
         /**
          * {@inheritDoc}
          */
         public function add(array $data = []):void {
-            $connection = $this->connectToDB();
-
-            $builder    = (new InsertQueryBuilder($this))
-                          ->set($data);
-
-            $connection->query($builder);
+            $this->connectToDB()->query(
+                (new InsertQueryBuilder($this))
+                ->set($data)
+            );
         }
 
         /**
          * {@inheritDoc}
          */
         public function edit(array $data = []):void {
-            $connection = $this->connectToDB();
-
-            $builder    = (new UpdateQueryBuilder($this))
-                            ->set($data)
-                            ->where("`" . $this->getDomainName() . "_id` = " . $data["id"]);
-
-            $connection->query($builder);
+            $this->connectToDB()->query(
+                (new UpdateQueryBuilder($this))
+                ->set($data)
+                ->where("`" . $this->getDomainName() . "_id` = " . $data["id"])
+            );
         }
 
         /**
          * {@inheritDoc}
          */
         public function delete(int $id):void {
-            $connection = $this->connectToDB();
             $domainName = $this->getDomainName();
 
-            $builder    = (new UpdateQueryBuilder($this))
-                          ->set([$domainName . "_is_deleted" => 1])
-                          ->where("`{$domainName}_id` = " . $id);
-
-            $connection->query($builder);
+            $this->connectToDB()->query(
+                (new UpdateQueryBuilder($this))
+                ->set([$domainName . "_is_deleted" => 1])
+                ->where("`{$domainName}_id` = " . $id)
+            );
         }
 
         /**
@@ -170,16 +156,10 @@ use DBQueries\InsertQueryBuilder;
          * @return int
          */
         public function calculateRecordCount():int {
-            $connection = $this->connectToDB();
-            $columns    = ["count" => "count(*)"];
-
-            $builder    = (new SelectQueryBuilder($this))
-                            ->what($columns);
-
-			$result = $connection->query($builder);
-            $count  = $result->fetch_assoc()["count"];
-            
-			return $count;
+			return $this->connectToDB()->query(
+                (new SelectQueryBuilder($this))
+                ->what(["count" => "count(*)"])
+            )->fetch_assoc()["count"];
         }
 
         /**
@@ -189,12 +169,12 @@ use DBQueries\InsertQueryBuilder;
          * @return int
          */
         public function getCurrentPageNumber(string $requestUri):int {
-			$uriParted  = explode('/', $requestUri);
-			$pageString = $uriParted[count($uriParted) - 1];
-			$pageParted = explode('=', $pageString);
-            $pageNumber = $pageParted[1] ?? 1;
+			$uriParted  = explode("/", $requestUri);
             
-			return $pageNumber;
+			return explode(
+                "=",
+                $uriParted[count($uriParted) - 1]
+            ) ?? 1;
         }
 
         /**
