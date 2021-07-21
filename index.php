8613a604d5e457a5a5a29526343969529dfce2f1
Max Goryunov <maximthehedgehog@yandex.ru>
2021-07-22 01:11:08 +0300

    small index.php change

--- index.php
+++ index.php
@@ -13,13 +13,5 @@
     include_once("./components/Autoloader.php");
     include_once("./config/constants.php");
 
-    $autoloader = new Autoloader();
-
-    /**
-     * Registering an autoloader function
-     */
-    $autoloader->register();
-
-    $router = new Router();
-
-    $router->run();
+    (new Autoloader())->register();
+    (new Router())->run();
