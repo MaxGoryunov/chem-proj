27f1a36ffd75c1fb0c371cc48d15ce43fa818fcc
Max Goryunov <maximthehedgehog@yandex.ru>
2021-08-15 23:10:15 +0300

    #103: added markets

--- http/Request.php
+++ http/Request.php
@@ -4,6 +4,9 @@ namespace Http;
 
 /**
  * Request interface.
+ * @todo #103:30min Add Request implementation. Let's add a class which
+ *  implements this interface.
+ * @todo #103:20min Make `uri` method return URI object.
  */
 interface Request
 {
